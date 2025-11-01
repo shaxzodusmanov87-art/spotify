50346a0949663f9ca14a1993d7e6bf1f851ae33b
shaxzodusmanov87-art <shaxzodusmanov87@gmail.com>
2025-11-01 10:32:42 +0500

    six commit

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const CLIENT_ID = "4250d84acd1c4588ae599ade84fbf69b";
 const REDIRECT_URI = "https://weproshakhzodspoty.netlify.app";
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const RESPONSE_TYPE = "code";
-const SCOPES = ["user-read-private", "user-read-email", "playlist-modify-public"];
+const SCOPE = "user-read-email user-read-private";
 
 function generateRandomString(length: number) {
   let text = "";
@@ -16,31 +14,35 @@ function generateRandomString(length: number) {
 }
 
 async function generateCodeChallenge(codeVerifier: string) {
-  const digest = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(codeVerifier));
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await crypto.subtle.digest("SHA-256", data);
   return btoa(String.fromCharCode(...new Uint8Array(digest)))
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
     .replace(/=+$/, "");
 }
 
+async function handleLogin() {
+  const codeVerifier = generateRandomString(128);
+  const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+  localStorage.setItem("code_verifier", codeVerifier);
+
+  const args = new URLSearchParams({
+    response_type: "code",
+    client_id: CLIENT_ID,
+    scope: SCOPE,
+    redirect_uri: REDIRECT_URI,
+    code_challenge_method: "S256",
+    code_challenge: codeChallenge,
+  });
+
+  window.location.href = `https://accounts.spotify.com/authorize?${args.toString()}`;
+}
+
 const Login: React.FC = () => {
 
-    const handleLogin = async () => {
-        const codeVerifier = generateRandomString(128);
-        const codeChallenge = await generateCodeChallenge(codeVerifier);
-    
-        localStorage.setItem("code_verifier", codeVerifier);
-    
-        const url = new URL(AUTH_ENDPOINT);
-        url.searchParams.append("client_id", CLIENT_ID);
-        url.searchParams.append("response_type", RESPONSE_TYPE);
-        url.searchParams.append("redirect_uri", REDIRECT_URI);
-        url.searchParams.append("scope", SCOPES.join(" "));
-        url.searchParams.append("code_challenge_method", "S256");
-        url.searchParams.append("code_challenge", codeChallenge);
-    
-        window.location.href = url.toString();
-      };
+   
 
     return (
         <div className="w-full h-screen bg-black flex justify-center items-center font-sans">
